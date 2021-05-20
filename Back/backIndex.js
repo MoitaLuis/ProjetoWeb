f8fb3be6ad07a86b0fdda4f3472869f31379a209
Luís <luismoita1337@gmail.com>
2021-05-20 20:32:16 -0300

    adição de outra collection

--- Back/backIndex.js
+++ Back/backIndex.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 9090;
 const mongoose = require('mongoose');
 const dataSchema = require('../Back/schema');
+const dataSchema2 = require('./textschema');
 const bodyParser = require('body-parser');
 var cors = require('cors');
 
@@ -27,6 +28,32 @@ app.post('/authenticate', (req,res) => {
   const post = new dataSchema({
     Email: req.body.Email,
     Senha: req.body.Senha
+  })
+
+  post.save()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json({message: err});
+    });
+});
+
+
+app.get('/uploads',function(req,res){
+  dataSchema2.find(function(err,usuario){
+   if(err){
+     console.log(err)
+     }  
+   else{
+     res.send(usuario)
+   }
+  })
+})
+
+app.post('/uploads', (req,res) => {
+  const post = new dataSchema2({
+    Conteudo: req.body.Conteudo
   })
 
   post.save()
